4d91c69a3d1ac60173b2cceb233e5c0a711c30f6
Mr-Anas1 <a3689211@gmail.com>
2025-08-24 19:09:43 +0530

    Fixed some bugs

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
-    base: "/Project-1/"
+  base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        service: resolve(__dirname, 'service.html'),
+      }
+    }
+  }
 })
